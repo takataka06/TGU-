6bceb918494b966f9da9d9e9ff1dbfbe2641e0ce
takataka06 <reyreyrey0110@gmail.com>
2025-06-29 04:16:03 +0900

    Add host

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
+    config.hosts << "tgu-site.onrender.com" # 追加
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
