63eead352f195049d1a3cd317fea9d454ac6263c
takataka06 <reyreyrey0110@gmail.com>
2025-06-29 00:18:02 +0900

    Add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts.clear
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -91,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "tgu-site.onrender.com"
+
 end
