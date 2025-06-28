548d41aee75c0646a48baad97a91af27f4cb36be
takataka06 <reyreyrey0110@gmail.com>
2025-06-29 03:54:16 +0900

    Add contact

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+
+
   def index
     @posts =Post.order(created_at: :desc)
     @post = Post.new
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts.clear
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -92,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
+  
 end
