2fd82591c6fb121e72218a4a9ede405cddeb783e
takataka06 <reyreyrey0110@gmail.com>
2025-09-12 01:09:43 +0900

    Add favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_14_045615) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_11_132146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
